d03484a96a44ccdc87b59ffd13e341863fc092da
SinchanaBPgit <160091520+SinchanaBPgit@users.noreply.github.com>
2024-05-28 14:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,21 @@ users = {
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/')
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # Authentication logic goes here
-        return redirect(url_for('dashboard'))
-    else:
-        return render_template('login.html')
+    return render_template('login.html')
+
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         # Authentication logic goes here
+#         return redirect(url_for('dashboard'))
+#     else:
+#         return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
